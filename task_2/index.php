9dbb1eb6f1fda14cf4e214c1dbd7e4e77a39f8cd
Dmitry V <dvoskoboev@action-media.ru>
2025-08-12 13:22:58 +0300

    Minor PHP stylistic changes

--- task_2/index.php
+++ task_2/index.php
@@ -3,25 +3,25 @@
 require_once './data/products.php';
 require_once './helpers/ArrayTransformer.php';
 
-const AGGREGATION_PROP_NAMES = [
+const AGGREGATION_FIELD_NAMES = [
   'name'   => 'name',
   'aspect'  => 'characteristic',
   'innerGroup'  => 'characteristics',
   'value'  => 'value'
 ];
 const SORTING_FIELD = 'name';
-const FILTERS_ARRAY = ['name', 'characteristic'];
+const FILTERS = ['name', 'characteristic'];
 
 $setAggregationGroupCallback = fn($p) => $p['archive'] ? 'archive' : 'notArchive';
 
 $productsArrayTransformer = new Helpers\ArrayTransformer(Data\PRODUCTS);
 
 $productsArrayTransformer->aggregateItemsFields(
-  AGGREGATION_PROP_NAMES,
+  AGGREGATION_FIELD_NAMES,
   $setAggregationGroupCallback,
 );
 $productsArrayTransformer->sortAggregatedFields(SORTING_FIELD);
-$productsArrayTransformer->transformIntoFiltersArray(FILTERS_ARRAY);
+$productsArrayTransformer->transformIntoFiltersArray(FILTERS);
 
 echo '<pre>';
 var_dump($productsArrayTransformer->getGroupsArray());
